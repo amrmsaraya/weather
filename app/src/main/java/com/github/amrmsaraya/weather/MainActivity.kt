f5304039d0e4bf53f68a037a1e60d1edbbe2c0ad
amrmsaraya <amrmsaraya@gmail.com>
2021-02-07 03:24:07 +0200

    add navigation drawer and map fragment

--- app/src/main/java/com/github/amrmsaraya/weather/MainActivity.kt
+++ app/src/main/java/com/github/amrmsaraya/weather/MainActivity.kt
@@ -1,19 +1,79 @@
 package com.github.amrmsaraya.weather
 
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.NavigatorProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.github.amrmsaraya.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navController: NavController
+    private var previousFragment = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set background colors
         window.statusBarColor = Color.parseColor("#FF1F1F1F")
         window.navigationBarColor = Color.parseColor("#FF1F1F1F")
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FF1F1F1F")))
+
+        navController = findNavController(R.id.fragment)
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        binding.navView.setCheckedItem(R.id.navHome)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            binding.drawerLayout.closeDrawers()
+            previousFragment++
+            when (it.itemId) {
+                R.id.navHome -> navController.navigate(R.id.homeFragment)
+                R.id.navFavorites -> navController.navigate(R.id.favoritesFragment)
+                R.id.navAlerts -> navController.navigate(R.id.alertsFragment)
+                R.id.navSettings -> navController.navigate(R.id.settingsFragment)
+            }
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(binding.navView)) {
+            binding.drawerLayout.closeDrawers()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
+
---
+++ app/src/main/java/com/github/amrmsaraya/weather/MapsFragment.kt
@@ -0,0 +1,58 @@
+package com.github.amrmsaraya.weather
+
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.github.amrmsaraya.weather.databinding.FragmentMapsBinding
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : Fragment() {
+    private lateinit var binding: FragmentMapsBinding
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var x = requireActivity() as AppCompatActivity
+        x.supportActionBar?.hide()
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_maps, container, false)
+        binding.btnMapSave.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mapsFragment_to_favoritesFragment)
+            x.supportActionBar?.show()
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
