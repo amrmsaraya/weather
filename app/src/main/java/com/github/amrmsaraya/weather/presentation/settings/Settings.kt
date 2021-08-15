02a805a2638a9309e51e22d8e27d3ec57f94faad
Amr Saraya <amrmsaraya@gmail.com>
2021-08-15 19:05:42 +0200

    remove unused domain package

--- app/src/main/java/com/github/amrmsaraya/weather/presentation/settings/Settings.kt
+++ app/src/main/java/com/github/amrmsaraya/weather/presentation/settings/Settings.kt
@@ -1,9 +1,50 @@
 package com.github.amrmsaraya.weather.presentation.settings
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Settings(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+    ) {
+        var isLanguageExpanded by remember { mutableStateOf(false) }
 
+        Text(text = "General", style = MaterialTheme.typography.h6)
+        Row() {
+            Text(text = "Language")
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.TopStart)) {
+
+                Button(onClick = { isLanguageExpanded = true }) {
+                    Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(text = "English")
+                        Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+                    }
+                }
+                DropdownMenu(
+
+                    expanded = isLanguageExpanded,
+                    onDismissRequest = { isLanguageExpanded = false }) {
+                    DropdownMenuItem(onClick = { /*TODO*/ }) {
+                        Text(text = "English")
+                    }
+                    DropdownMenuItem(onClick = { /*TODO*/ }) {
+                        Text(text = "Arabic")
+                    }
+                }
+            }
+        }
+    }
 }
--- domain/src/main/java/com/github/amrmsaraya/domain/WeatherRepo.kt
+++
@@ -1,6 +0,0 @@
-package com.github.amrmsaraya.domain
-
-interface WeatherRepo {
-
-    suspend fun getWeather(): Forecast
-}
